73196b4a69f93003a9b99c3836bffc61650c5774
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-16 16:58:00 +0300

    Добавил вторую задачу

---
+++ Exs2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет*/
+int number = ReadInt("Введите число: ");
+if (number < 0)
+    number = - number;
+if (number < 100 )
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    while (number > 999)
+       {
+        number = number/10;
+       } 
+    int SecondNumber = number % 10;
+    Console.Write(SecondNumber);
+}
+int ReadInt(string messadge)
+{
+    Console.Write(messadge);
+    return Convert.ToInt32(Console.ReadLine());
+}
