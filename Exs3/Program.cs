955e0594777f252ee5d5c3fa4706ac36dadbdd1e
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-16 17:19:50 +0300

    Добавил третью задачу

---
+++ Exs3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ и проверяет, является ли этот день выходным*/ 
+ int number = ReadInt("Введите цифру, обозначающую день недели: ");
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Введена неверная цифра");
+    
+}
+else
+{
+    if (number == 6 || number == 7 )
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+    Console.Write("Нет");
+    }
+}
+int ReadInt(string messadge)
+{
+    Console.Write(messadge);
+    return Convert.ToInt32(Console.ReadLine());
+}
