93c2f451de93ec5158d1aa3208fc212d3482ff45
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-16 16:16:28 +0300

    Добавил первую задачу

---
+++ Exs1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа */
+
+int number = ReadInt("Введите трехзначное число: ");
+if (number < 0)
+    number = - number;
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    int SecondNumber = (number % 100 - number % 10) / 10;
+    Console.Write(SecondNumber);
+}
+int ReadInt(string messadge)
+{
+    Console.Write(messadge);
+    return Convert.ToInt32(Console.ReadLine());
+}
